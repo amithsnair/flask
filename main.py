6fbef99db9a89d26d3ef25b832d1d98d4cf2ba62
amithsnair <amithsopanam@gmail.com>
2023-01-11 17:24:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,12 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify
 import os
-import pickle
-import numpy as np
-
-# Load the Random Forest CLassifier model
-filename = 'heart-disease-prediction-knn-model.pkl'
-model = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
+
 
 @app.route('/')
-def home():
-	return render_template('main.html')
-
-
-@app.route('/predict', methods=['GET','POST'])
-def predict():
-    if request.method == 'POST':
-
-        age = int(request.form['age'])
-        sex = request.form.get('sex')
-        cp = request.form.get('cp')
-        trestbps = int(request.form['trestbps'])
-        chol = int(request.form['chol'])
-        fbs = request.form.get('fbs')
-        restecg = int(request.form['restecg'])
-        thalach = int(request.form['thalach'])
-        exang = request.form.get('exang')
-        oldpeak = float(request.form['oldpeak'])
-        slope = request.form.get('slope')
-        ca = int(request.form['ca'])
-        thal = request.form.get('thal')
-        
-        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-        my_prediction = model.predict(data)
-        
-        return render_template('result.html', prediction=my_prediction)
-             
-
-app = Flask(__name__)
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
 if __name__ == '__main__':
